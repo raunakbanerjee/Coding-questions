f492823948519ca463803c47b093ea28cc0e0ee8
raunakbanerjee <76702613+raunakbanerjee@users.noreply.github.com>
2021-07-14 02:26:09 +0530

    Update 2.HighestOcuuringChar.cpp

--- Hashmaps/2.HighestOcuuringChar.cpp
+++ Hashmaps/2.HighestOcuuringChar.cpp
@@ -3,6 +3,9 @@ So I'll basically store all of em in the hashmap and then find out whose freq is
 
 */
 
+I/P--> abbca
+O/P--> a
+
 char getMaxOccuringChar(string str)
     {
         int max=0;
@@ -18,3 +21,36 @@ char getMaxOccuringChar(string str)
         }
         }
         return ans;
+
+    
+Q2. Number of occurence of highes occuring character
+    I/P--> abbca
+    O/P--> 3 (as a occurs thrice)
+    
+    
+ #include <iostream>
+    using namespace std;
+
+    int MostFrequentCharaceter(char str[]){
+   unordered_map<char,int> map1;
+  if(str[0]=='\0')
+    return -1;
+  int n=strlen(str);
+        for(int i=0;i<n;i++)
+        map1[str[i]]++; 
+  int max=0;
+  int ans;
+  for(auto it=map1.begin();it!=map1.end();it++){  
+    if(it->second>max)
+      max=it->second;
+  ans=max;
+  }
+  return ans;
+}
+    
+int main(){
+  char s[100];
+  cin>>s;
+  cout<<MostFrequentCharaceter(s);
+}
+    
